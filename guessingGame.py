0708b5f8f08302d593ccdada976b5b14eb06d967
Yasin Hussein <bulaale116@gmail.com>
2024-06-10 09:57:52 -0600

    Display the game stats .

--- guessingGame.py
+++ guessingGame.py
@@ -1,36 +1,49 @@
 import random
 print('welcome to guessing game ')
-game_on = True
 num = random.randint(1,10)
-print(num)
 guessed = []
 play_again=True
-while play_again:
+game_on = True
+allowed_attempts = 3
+games_played=1
+number_of_wins = 0
+while game_on:
     
-  while game_on:
+  while allowed_attempts > 0:
     gues = int(input('Gues a number between 1 and 10 :'))
     if gues not in guessed:
       if gues > num:
 
         print(gues ,'is too high')
         guessed.append(gues)
+        allowed_attempts -=1
       
       elif gues<num:
         print(gues ,'is too low')
         guessed.append(gues)
+        allowed_attempts -=1
       else:
         print('💥💥 You correctly guessed the number : ')
-        game_on = False
+        allowed_attempts=0
+        number_of_wins +=1
+
     else:
       print('Already guessed !')
+
   again = input('Would you like to play again Y/N :')
+
   if again == 'Y' or again == 'y':
-    game_on=True
     num = random.randint(1,10)
+    allowed_attempts=3
+    games_played +=1
     guessed=[]
   else:
-    play_again = False
+    game_on = False
+    print('You played :',games_played)
+    print('You won :',number_of_wins)
+    print('You lost :',games_played - number_of_wins)
     print('End of the game ...')
+    print('Thanks👍🏽👍🏽👍🏽')
 
 
 
