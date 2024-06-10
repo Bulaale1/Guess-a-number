f0d1e8aaa876a8cc20ec3b84d6abf9477b5a3065
Yasin Hussein <bulaale116@gmail.com>
2024-06-10 09:12:24 -0600

    play again logic is implemented .

--- guessingGame.py
+++ guessingGame.py
@@ -4,24 +4,33 @@ game_on = True
 num = random.randint(1,10)
 print(num)
 guessed = []
-while game_on:
-
-  gues = int(input('Gues a number between 1 and 10 :'))
-  if gues not in guessed:
-    if gues > num:
-
-      print(gues ,'is too high')
-      guessed.append(gues)
+play_again=True
+while play_again:
     
-    elif gues<num:
-      print(gues ,'is too low')
-      guessed.append(gues)
+  while game_on:
+    gues = int(input('Gues a number between 1 and 10 :'))
+    if gues not in guessed:
+      if gues > num:
+
+        print(gues ,'is too high')
+        guessed.append(gues)
+      
+      elif gues<num:
+        print(gues ,'is too low')
+        guessed.append(gues)
+      else:
+        print('💥💥 You correctly guessed the number : ')
+        game_on = False
     else:
-      print('💥💥 You correctly guessed the number : ')
-      game_on = False
+      print('Already guessed !')
+  again = input('Would you like to play again Y/N :')
+  if again == 'Y' or again == 'y':
+    game_on=True
+    num = random.randint(1,10)
+    guessed=[]
   else:
-    print('Already guessed !')
-print('End of the game ...')
+    play_again = False
+    print('End of the game ...')
 
 
 
