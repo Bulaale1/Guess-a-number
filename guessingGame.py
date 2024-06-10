09484a29ff1cbc7336beb48217826bca18ef200f
Yasin Hussein <bulaale116@gmail.com>
2024-06-10 08:53:01 -0600

    game logic is implemented

--- guessingGame.py
+++ guessingGame.py
@@ -1,4 +1,27 @@
 import random
-num = random.randint(1,10)
 print('welcome to guessing game ')
+game_on = True
+num = random.randint(1,10)
+print(num)
+guessed = []
+while game_on:
+
+  gues = int(input('Gues a number between 1 and 10 :'))
+  if gues not in guessed:
+    if gues > num:
+
+      print(gues ,'is too high')
+      guessed.append(gues)
+    
+    elif gues<num:
+      print(gues ,'is too low')
+      guessed.append(gues)
+    else:
+      print('💥💥 You correctly guessed the number : ')
+      game_on = False
+  else:
+    print('Already guessed !')
+print('End of the game ...')
+
+
 
